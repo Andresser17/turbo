7a40f42a11c534ac79bcfbd7b18e198bf8415136
Andresser17 <andresserserrano2020@gmail.com>
2022-10-19 18:36:06 -0400

    build: rename namespace from Turbo to Andresser17/Turbo

--- src/Response.php
+++ src/Response.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Turbo;
+namespace Andresser17\Turbo;
 
 class Response
 {
--- src/Router.php
+++ src/Router.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Turbo;
+namespace Andresser17\Turbo;
 
 use \Turbo\Route;
 use \Turbo\Request;
